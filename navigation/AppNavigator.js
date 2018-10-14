43389c384176d8bddfd9d81ebef36c47eef4cdbe
amar agya <>
2018-10-14 18:07:59 +0700

    merubah komentar dan nama navigasi

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,7 +4,7 @@ import { createSwitchNavigator } from 'react-navigation';
 import MainTabNavigator from './MainTabNavigator';
 
 export default createSwitchNavigator({
-  // You could add another route here for authentication.
+  // You could add another route here for authentication.halohalo
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
 });
